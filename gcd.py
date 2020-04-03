1128cabe0498fe20e7001fe77da52126e966f4e0
Thomas-Malcolm <ThomasMalcolm01@gmail.com>
2020-04-04 00:15:42 +1000

    gcd algorithm

---
+++ gcd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+
+def help():
+	print("usage: gcd [a] [b]")
+	exit()
+
+def gcd(a, b):
+
+	if a == 0:
+		return b
+
+	if b == 0:
+		return a
+
+	return gcd(b, a % b)
+
+def main():
+
+	if len(sys.argv) != 3:
+		help()
+
+	try:
+		a = int(sys.argv[1])
+		b = int(sys.argv[2])
+	except:
+		help()
+
+	gcd_val = gcd(a, b)
+
+	print("gcd({},{}) = {}".format(a, b, gcd_val))
+
+if __name__ == "__main__":
+	main()
